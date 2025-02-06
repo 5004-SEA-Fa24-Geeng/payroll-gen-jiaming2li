4debc322bed5da2e1b15ec1e1be0415fc9aa419b
jiamingli <li.jiaming2@northeastern.edu>
2025-02-06 13:24:59 -0800

    hw03

--- src/main/java/student/SalaryEmployee.java
+++ src/main/java/student/SalaryEmployee.java
@@ -64,7 +64,7 @@ public class SalaryEmployee implements IEmployee{
     public String toCSV() {
         return String.format("%s,%s,%s,%.2f,%.2f,%.2f,%.2f",
                 //getEmployeeType(), getName(), getID(), getPayRate(), getPretaxDeductions(), getYTDEarnings()+h_pay-h_taxes-getPretaxDeductions(), getYTDTaxesPaid()+h_taxes);
-                getEmployeeType(), getName(), getID(), getPayRate(), getYTDEarnings()+h_pay-h_taxes-getPretaxDeductions(), getYTDTaxesPaid()+h_taxes, getPretaxDeductions());
+                getEmployeeType(), getName(), getID(), getPayRate(), getYTDTaxesPaid()+h_taxes, getYTDEarnings()+h_pay-h_taxes-getPretaxDeductions(), getYTDTaxesPaid()+h_taxes);
 
     }
 }
