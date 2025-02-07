a264fc5d1239bc0052ba81645c1645fa2aa1d065
jiamingli <li.jiaming2@northeastern.edu>
2025-02-06 17:02:44 -0800

    hw03

--- src/main/java/student/HourlyEmployee.java
+++ src/main/java/student/HourlyEmployee.java
@@ -71,6 +71,7 @@ public class HourlyEmployee implements IEmployee{
     @Override
     public String toCSV() {
         return String.format("%s,%s,%s,%.2f,%.2f,%.2f,%.2f",
-                getEmployeeType(), getName(), getID(), getPayRate(), getPretaxDeductions(), ytdEarnings, ytdTaxesPaid);
+                //getEmployeeType(), getName(), getID(), getPayRate(), getPretaxDeductions(), ytdEarnings, ytdTaxesPaid);
+                getEmployeeType(), getName(), getID(), getPayRate(), ytdEarnings, getPretaxDeductions(), ytdTaxesPaid);
     }
 }
